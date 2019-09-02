01c3e25f804d9d86bfd546d6830e86566cba9a94
IceMistWorm <digtorio@gmail.com>
2019-09-02 09:34:10 -0400

    update drawstuff.js for two functions

--- drawstuff.js
+++ drawstuff.js
@@ -128,6 +128,27 @@ function getInputTriangles() {
         return JSON.parse(httpReq.response); 
 } // end get input triangles
 
+//get the input boxex from the standard class URL
+function getInputBoxes() {
+    const INPUT_BOXES_URL = 
+        "https://ncsucgclass.github.io/prog1/boxes.json";
+        
+    // load the boxes file
+    var httpReq = new XMLHttpRequest(); // a new http request
+    httpReq.open("GET",INPUT_BOXES_URL,false); // init the request
+    httpReq.send(null); // send the request
+    var startTime = Date.now();
+    while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
+        if ((Date.now()-startTime) > 3000)
+            break;
+    } // until its loaded or we time out after three seconds
+    if ((httpReq.status !== 200) || (httpReq.readyState !== XMLHttpRequest.DONE)) {
+        console.log*("Unable to open input boxes file!");
+        return String.null;
+    } else
+        return JSON.parse(httpReq.response); 
+} // end get input boxes
+
 // put random points in the ellipsoids from the class github
 function drawRandPixelsInInputEllipsoids(context) {
     var inputEllipsoids = getInputEllipsoids();
@@ -343,6 +364,99 @@ function drawInputTrainglesUsingPaths(context) {
     } // end if triangle files found
 } // end draw input triangles
 
+// put random points in the boxes from the class github
+function drawRandPixelsInInputBoxes(context) {
+    var inputBoxes = getInputBoxes();
+    var w = context.canvas.width;
+    var h = context.canvas.height;
+    var imagedata = context.createImageData(w,h);
+    const PIXEL_DENSITY = 0.1;
+    var numCanvasPixels = (w*h)*PIXEL_DENSITY; 
+    
+    if (inputBoxes != String.null) { 
+	    var x  = 0; var y  = 0; // pixel coord init
+        var lx = 0; var rx = 0; // input lx, rx from boxes.json
+        var by = 0; var ty = 0; // input by, ty from boxes.json
+        var fz = 0; var rz = 0; // input fz, rz from boxes.json
+        var numBoxPixels = 0; // init num pixels in boxes
+        var c = new Color(0,0,0,0); // init the box color
+        var n = inputBoxes.length; // the number of input boxes
+        //console.log("number of ellipses: " + n);
+
+        // Loop over the ellipsoids, draw rand pixels in each
+        for (var b=0; b<n; b++) {
+			// input lx,rx,by,ty on canvas
+			lx = w*inputBoxes[b].lx;
+			rx = w*inputBoxes[b].rx;
+			by = h*inputBoxes[b].by;
+			ty = h*inputBoxes[b].ty;           
+			
+            numBoxesPixels  = (rx-lx)*(ty-by); // projected box area 
+            numBoxesPixels *= PIXEL_DENSITY;  // percentage of box area to render to pixels
+            numBoxesPixels  = Math.round(numBoxesPixels);
+           
+            //console.log("num box pixels: "+numBoxesPixels);
+            
+			c.change(
+                inputBoxes[b].diffuse[0]*255,
+                inputBoxes[b].diffuse[1]*255,
+                inputBoxes[b].diffuse[2]*255,
+                255); // box diffuse color
+            for (var p=0; p<numBoxesPixels; p++) {
+                do {
+                    x = Math.floor(Math.random()*w); 
+                    y = Math.floor(Math.random()*h); 
+                } while ( x<lx || x>rx || y>ty || y<by ) // inside the projection
+                drawPixel(imagedata,x,y,c);
+                //console.log("color: ("+c.r+","+c.g+","+c.b+")");
+                //console.log("x: " + x);
+                //console.log("y: " + y);
+            } // end for pixels in box
+        } // end for boxes
+        context.putImageData(imagedata, 0, 0);
+    } // end if boxes found
+} // end draw rand pixels in input boxes
+
+//draw 2d projections boxes from the JSON file at class github
+function drawInputBoxesUsingPaths(context) {
+    var inputBoxes = getInputBoxes();
+    var n = inputBoxes.length; // the number of input boxes
+	
+    if (inputBoxes != String.null) { 
+		var w = context.canvas.width;
+        var h = context.canvas.height;
+        var c = new Color(0,0,0,0); // the color at the pixel: black
+        var x  = 0; var y  = 0; // pixel coord init
+        var lx = 0; var rx = 0; // input lx, rx from boxes.json
+        var by = 0; var ty = 0; // input by, ty from boxes.json
+        var fz = 0; var rz = 0; // input fz, rz from boxes.json
+        //console.log("number of files: " + n);
+
+        // Loop over the input files
+        for (var b=0; b<n; b++) {
+				
+			// input lx,rx,by,ty on canvas
+			lx = w*inputBoxes[b].lx;
+			rx = w*inputBoxes[b].rx;
+			by = h*inputBoxes[b].by;
+			ty = h*inputBoxes[b].ty; 
+        		
+            context.fillStyle = 
+            	"rgb(" + Math.floor(inputBoxes[b].diffuse[0]*255)
+            	+","+ Math.floor(inputBoxes[b].diffuse[1]*255)
+            	+","+ Math.floor(inputBoxes[b].diffuse[2]*255) +")"; // diffuse color
+            
+            var path=new Path2D();
+            path.moveTo(lx,ty);
+            path.lineTo(lx,by);
+            path.lineTo(rx,by);
+			path.lineTo(rx,ty);
+            path.closePath();
+            context.fill(path);
+
+        } // end for files
+    } // end if box files found
+} // end draw input boxes
 
 /* main -- here is where execution begins after window load */
 
@@ -362,9 +476,15 @@ function main() {
     //drawInputEllipsoidsUsingArcs(context);
       // shows how to read input file, but not how to draw pixels
     
-    drawRandPixelsInInputTriangles(context);
+    //drawRandPixelsInInputTriangles(context);
     // shows how to draw pixels and read input file
     
     //drawInputTrainglesUsingPaths(context);
+    // shows how to read input file, but not how to draw pixels
+    
+    drawRandPixelsInInputBoxes(context);
+	// shows how to draw pixels and read input file
+    
+	//drawInputBoxesUsingPaths(context);
     // shows how to read input file, but not how to draw pixels
 }
