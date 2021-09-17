c1e8a15178b180fda57b492a96d5bf420b380a9f
dr-doc-phd <74687769+dr-doc-phd@users.noreply.github.com>
2021-09-17 12:19:00 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -365,6 +365,7 @@ function drawUnlitTriangles(context) {
         // Loop over the pixels
         for (var e=0; e<w; e++) {
             for (var g=0; g<h; g++) {
+                drawPixel(imagedata,e,g,c);
                 //calculate screen pixel's location in simulated space
                 var s = g/(h-1);
                 var t = e/(w-1);
@@ -422,7 +423,6 @@ function drawUnlitTriangles(context) {
                         var NORM = [( BA[1] * CA[2] - BA[2] * CA[1]), (BA[2] * CA[0] - BA[0] * CA[2]), (BA[0] * CA[1] - BA[1] * CA[0])] ;
                         var d = (NORM[0]*vertexPos2[0]) + (NORM[1]*vertexPos2[1]) + (NORM[2]*vertexPos2[2]);
                         var check = (NORM[0]*D[0]) + (NORM[1]*D[1]) + (NORM[2]*D[2]);
-                        drawPixel(imagedata,e,g,c);
                         if (check != 0) {
                             var NORMe = (NORM[0]*eye[0]) + (NORM[1]*eye[1]) + (NORM[2]*eye[2]);
                             var distance = (d-NORMe)/check;
